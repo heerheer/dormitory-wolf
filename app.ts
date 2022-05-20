047439b4693fb4020a3ccc9663ed4d30f9df42cb
HeerKaisair <1767407822@qq.com>
2022-05-20 22:18:24 +0800

    优化女巫卡片,新增新的模式,即将开始制作房内WS优化

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import KoaRouter from "koa-router"
 import KoaBody from 'koa-body'
 import {Room} from "./types";
 import * as fs from "fs";
+import os from 'os'
 // @ts-ignore
 import KoaStatic from "koa-static";
 
@@ -15,8 +16,29 @@ app.use(KoaBody({
 const rooms: { [key: string]: Room | undefined } = {}
 
 //初始化一个房间。
-function loadRoom(id: string) {
-    rooms[id] = {players: [], pool: ["平民", "平民", "狼人", "狼人", "女巫", "预言家"]}
+function loadRoom(id: string, mode: 'wolf' | 'undercover') {
+    if (mode == 'wolf') {
+        rooms[id] = {id, players: [], pool: ["平民", "平民", "狼人", "狼人", "女巫", "预言家"], mode}
+    }
+    if (mode == 'undercover') {
+        let lines = fs.readFileSync('./words.txt').toString().split(os.EOL).filter(x => x != '');
+        let line = lines[Math.floor(Math.random() * lines.length)];
+        let a = line.split(',')[0];
+        let b = line.split(',')[1];
+        rooms[id] = {
+            id,
+            players: [],
+            pool: [],
+            mode,
+            undercover_data: {true_word: a, undercover_count: 2, blank_count: 0, normal_count: 4, undercover_word: b}
+        }
+        for (let i = 0; i < rooms[id]!.undercover_data!.normal_count; i++) {
+            rooms[id]?.pool.push(a)
+        }
+        for (let i = 0; i < rooms[id]!.undercover_data!.undercover_count; i++) {
+            rooms[id]?.pool.push(b)
+        }
+    }
 }
 
 //返回视图
@@ -25,8 +47,6 @@ router.get("/view", async (ctx) => {
     ctx.body = fs.readFileSync('mobile.html')
     return;
 })
-
-
 /**
  * post方法表示加入一个房间
  */
@@ -35,7 +55,29 @@ router.post("/wolf/:id", async (ctx, next) => {
     let id = ctx.params.id;
 
     if (!rooms[id]) {
-        loadRoom(id);
+        loadRoom(id, 'wolf');
+    }
+
+    let room = rooms[id]!;
+    let user = JSON.parse(ctx.request.body).user ?? "未命名";
+    let uid = JSON.parse(ctx.request.body).uid;
+    //获取一个随机角色
+    let role = room.pool[Math.floor(Math.random() * room.pool.length)];
+    //在pool角色池中移除角色
+    room.pool.splice(room.pool.indexOf(role), 1)
+    //将角色添加进room,这里等同于user:user role:role
+    room.players.push({user, role, uid})
+    //返回数据
+    ctx.body = {code: 0, role, left: room.pool.length};
+})
+
+
+router.post("/undercover/:id", async (ctx, next) => {
+    console.log(ctx.request.body)
+    let id = ctx.params.id;
+
+    if (!rooms[id]) {
+        loadRoom(id, 'wolf');
     }
 
     let room = rooms[id]!;
@@ -59,7 +101,17 @@ router.get("/wolf/:id", async (ctx) => {
     let id = ctx.params.id;
     if (!rooms[id]) {
         //若房间不存在，自动创建
-        loadRoom(id);
+        loadRoom(id, 'wolf');
+    }
+    let room = rooms[id];//当前room
+    ctx.body = {code: 0, room};
+})
+
+router.get("/undercover/:id", async (ctx) => {
+    let id = ctx.params.id;
+    if (!rooms[id]) {
+        //若房间不存在，自动创建
+        loadRoom(id, 'undercover');
     }
     let room = rooms[id];//当前room
     ctx.body = {code: 0, room};
@@ -68,15 +120,20 @@ router.get("/wolf/:id", async (ctx) => {
 /**
  * delete方法删除一个房间
  */
-router.delete("/wolf/:id", async (ctx) => {
+router.delete("/rooms/:id", async (ctx) => {
     let id = ctx.params.id;
     rooms[id] = undefined
     delete rooms[id] //删除属性
     ctx.body = {code: 0};
 })
 
-router.get("/wolf", async (ctx) => {
-    ctx.body = {code: 0, rooms}
+//获取房间列表
+router.get("/rooms", async (ctx) => {
+    let array: Room[] = []
+    Object.keys(rooms).forEach(x => {
+        array.push(rooms[x]!)
+    })
+    ctx.body = {code: 0, rooms: array}
 })
 
 
--- types.ts
+++ types.ts
@@ -1,4 +1,17 @@
 export interface Room {
+    id: string;
+    //已加入玩家
     players: Array<{ user: string, role: string, uid: string }>;
-    pool: Array<string>
+    pool: Array<string>;
+    mode: 'wolf' | 'undercover';
+    undercover_data?: UnderCoverData;
+    wolf_data?: UnderCoverData
+}
+
+export interface UnderCoverData {
+    true_word: string,
+    undercover_word: string,
+    undercover_count: number,
+    normal_count: number,
+    blank_count: number
 }
